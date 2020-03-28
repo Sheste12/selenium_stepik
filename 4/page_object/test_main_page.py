030f4319801d666892bc3e6cf170ef7794da9f94
sheste12 <sheste12@gmail.com>
2020-03-28 13:49:26 +0300

    Add login test in main test

--- 4/page_object/test_main_page.py
+++ 4/page_object/test_main_page.py
@@ -1,13 +1,11 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from selenium.webdriver import Remote
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page(browser: Remote):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
     page.go_to_login_page()
-    login_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    login_page = LoginPage(browser, login_link)
-    login_page.open()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
