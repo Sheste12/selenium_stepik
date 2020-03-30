05a24df9e897551e2059440f4e26929e8562cf5e
sheste12 <sheste12@gmail.com>
2020-03-30 16:49:03 +0300

    Refactoring

---
+++ 4/page_object/links.py
@@ -0,0 +1,39 @@
+import pytest
+
+
+class Links:
+    PRODUCT_PAGES = (
+        "link",
+        [
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+            pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                         marks=pytest.mark.xfail),
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+        ])
+    FIRST_PRODUCT_PAGE = (
+        "link",
+        ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"]
+    )
+    SECOND_PRODUCT_PAGE = (
+        "link",
+        ["http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"]
+    )
+    THIRD_PRODUCT_PAGE = (
+        "link",
+        ["http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"]
+    )
+    LOGIN_PAGE = (
+        "link",
+        ["http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"]
+    )
+    MAIN_PAGE = (
+        "link",
+        ["http://selenium1py.pythonanywhere.com/"]
+    )
--- 4/page_object/test_product_page.py
+++ 4/page_object/test_product_page.py
@@ -1,3 +1,5 @@
+# Все ссылки на странице находятся в отдельном конфиге
+from .links import Links
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
@@ -5,65 +7,43 @@ from selenium.webdriver import Remote
 import pytest
 import time
 
-@pytest.mark.skip()
-@pytest.mark.parametrize(
-    'link',
-    [
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
-])
+@pytest.mark.parametrize(*Links.PRODUCT_PAGES)
 class TestProductPage:
-    @pytest.mark.skip(reason="test_guest_can_add_product_to_basket")
     def test_guest_can_add_product_to_basket(self, browser: Remote, link):
         page = ProductPage(browser, link)
         page.open()
         page.click_button_add_to_basket()
 
-    def test_guest_cant_see_success_message_after_adding_product_to_basket(sefl, browser : Remote, link):
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser: Remote, link):
         page = ProductPage(browser, link, 0)
         page.open()
         page.click_button_add_to_basket()
-        # time.sleep(60)
         page.check_if_page_has_not_success_message()
 
-    @pytest.mark.skip()
-    def test_guest_cant_see_success_message(self, browser : Remote, link):
+    def test_guest_cant_see_success_message(self, browser: Remote, link):
         page = ProductPage(browser, link)
         page.open()
         page.check_if_page_has_not_success_message()
 
-    @pytest.mark.skip()
-    def test_message_disappeared_after_adding_product_to_basket(self, browser : Remote, link):
+    def test_message_disappeared_after_adding_product_to_basket(self, browser: Remote, link):
         page = ProductPage(browser, link)
         page.open()
         page.click_button_add_to_basket()
         page.check_if_page_has_success_message()
 
-@pytest.mark.parametrize(
-    "link",
-    [
-        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
-    ])
+@pytest.mark.parametrize(*Links.FIRST_PRODUCT_PAGE)
 class TestUserAddToBasketFromProductPage:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser : Remote):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        link = Links.LOGIN_PAGE
         page = LoginPage(browser, link)
         page.open()
         page.should_be_login_page()
         email = str(time.time()) + "@fakemail.org"
-        password = "aslkdfjhsadkljj52345adflshad"
+        password = "password_for_testing13"
         page.register_new_user(email, password)
 
-    def test_user_cant_see_success_message(self, browser : Remote, link):
+    def test_user_cant_see_success_message(self, browser: Remote, link):
         page = ProductPage(browser, link)
         page.open()
         page.check_if_page_has_not_success_message()
@@ -73,26 +53,23 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.click_button_add_to_basket()
 
-@pytest.mark.skip()
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
-@pytest.mark.skip()
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+@pytest.mark.parametrize(*Links.SECOND_PRODUCT_PAGE)
+class TestLoginPageFromProductPage:
+    def test_guest_should_see_login_link_on_product_page(self, browser: Remote, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-@pytest.mark.skip()
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_not_have_products()
-    basket_page.should_have_empty_basket_message()
+    def test_guest_can_go_to_login_page_from_product_page(self, browser: Remote, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser: Remote, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_basket()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_not_have_products()
+        basket_page.should_have_empty_basket_message()
