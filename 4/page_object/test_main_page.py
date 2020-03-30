0292c14e5cb2bc8ace860e3b9b8ad784cd37535b
sheste12 <sheste12@gmail.com>
2020-03-30 14:39:07 +0300

    Add settings

--- 4/page_object/test_main_page.py
+++ 4/page_object/test_main_page.py
@@ -4,20 +4,21 @@ from .pages.basket_page import BasketPage
 from selenium.webdriver import Remote
 import pytest
 
-@pytest.mark.skip()
-def test_guest_can_go_to_login_page(browser: Remote):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser: Remote):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser : Remote):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_not_have_products()
-    basket_page.should_have_empty_basket_message()
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser : Remote):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_basket()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_not_have_products()
+        basket_page.should_have_empty_basket_message()
